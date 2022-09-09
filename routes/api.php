c1090f9143d3ccd0f3d25fe07e213d6374c4c07d
Luis Murrugarra <luismurrugarra17@gmail.com>
2022-09-08 19:20:24 -0500

    ADD:Tests and Controllers, List Resources

--- routes/api.php
+++ routes/api.php
@@ -3,7 +3,24 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\{
-    AuthController
+    AuthController,
+    ArticleController,
+    ArticleTypeController,
+    AttendanceSheetController,
+    BankController,
+    DocumentTypeController,
+    EmployeeController,
+    ImageController,
+    MachineController,
+    MaintenanceSheetController,
+    MaintenanceTypeController,
+    NotificationController,
+    PositionController,
+    SupplierController,
+    SupplierTypeController,
+    UserController,
+    WorkingSheetController
+
 };
 
 /*
@@ -21,6 +38,23 @@ Route::prefix('v1/')->group(function () {
 
     Route::middleware(['auth:sanctum'])->group(function () {
         Route::post('logout', [AuthController::class, 'logout']);
+
+        Route::get('articles', [ArticleController::class, 'index']);
+        Route::get('article-types', [ArticleTypeController::class, 'index']);
+        Route::get('attendance-sheets', [AttendanceSheetController::class, 'index']);
+        Route::get('banks', [BankController::class, 'index']);
+        Route::get('document-types', [DocumentTypeController::class, 'index']);
+        Route::get('employees', [EmployeeController::class, 'index']);
+        Route::get('images', [ImageController::class, 'index']);
+        Route::get('machines', [MachineController::class, 'index']);
+        Route::get('maintenance-sheets', [MaintenanceSheetController::class, 'index']);
+        Route::get('maintenance-types', [MaintenanceTypeController::class, 'index']);
+        Route::get('notifications', [NotificationController::class, 'index']);
+        Route::get('positions', [PositionController::class, 'index']);
+        Route::get('suppliers', [SupplierController::class, 'index']);
+        Route::get('supplier-types', [SupplierTypeController::class, 'index']);
+        Route::get('users', [UserController::class, 'index']);
+        Route::get('working-sheets', [WorkingSheetController::class, 'index']);
     });
 });
 
