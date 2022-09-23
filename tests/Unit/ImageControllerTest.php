e8850fdee3f88ee74d64cb0915ea57c2fed18b76
Luis Murrugarra <luismurrugarra17@gmail.com>
2022-09-23 00:21:47 -0500

    UPDATE:ImageControllerTest - change metohnds

--- tests/Unit/ImageControllerTest.php
+++ tests/Unit/ImageControllerTest.php
@@ -13,23 +13,7 @@ class ImageControllerTest extends TestCase
     use RefreshDatabase;
 
     private $resource = 'images';
-
-    public function test_index()
-    {
-        $this->withoutExceptionHandling();
-        $user = User::factory()->create([
-            'email' => 'admin@jextecnologies.com',
-            'password' => bcrypt('123456')
-        ]);
-        $response = $this->actingAs($user)->withSession(['banned' => false])
-            ->getJson("api/v1/$this->resource");
-
-        $response->assertStatus(200)
-            ->assertJsonStructure(['data' => []]);
-
-    }
-
-    public function testAvatarUpload()
+    public function test_upload()
     {
         $this->withoutExceptionHandling();
         $user = User::factory()->create([
