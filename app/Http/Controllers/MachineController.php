372dcb5d29d2509ddd905818107b923d68edc88b
Luis Murrugarra <luismurrugarra17@gmail.com>
2022-09-23 00:09:08 -0500

    UPDATE:MachineController - tecnical sheet

--- app/Http/Controllers/MachineController.php
+++ app/Http/Controllers/MachineController.php
@@ -6,6 +6,7 @@ use App\Http\Requests\MachineRequest;
 use App\Http\Requests\MachineUpdateRequest;
 use App\Http\Resources\MachineResource;
 use App\Http\Resources\MachinetDetailResource;
+use App\Models\Article;
 use App\Models\Machine;
 use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
@@ -41,6 +42,8 @@ class MachineController extends Controller
 //          CREATE MACHINE
             $machine = Machine::create($request->except(['articles', 'image']));
             $this->addImage($machine, $request->image);
+            $this->addTechnicalSheet($machine, $request->technical_sheet);
+
 //            ATTACH ARTICLES
             $articles = [];
             array_map(function ($article) use (&$articles) {
@@ -66,6 +69,12 @@ class MachineController extends Controller
         $machine->image()->create(['path' => $path]);
 
     }
+    public function addTechnicalSheet(Machine $machine, $path)
+    {
+        if (!$path) return;
+        $machine->technical_sheet()->create(['path' => $path]);
+
+    }
 
     public function updateImage(Machine $machine, $path)
     {
@@ -77,6 +86,17 @@ class MachineController extends Controller
         if ($path == $machine->image->path) return;
         if (Storage::exists("public/" . $machine->image->path)) Storage::delete("public/" . $machine->image->path);
         $machine->image()->update(['path' => $path]);
+    }
+    public function updateTechnicalSheet(Machine $machine, $path)
+    {
+        if (!$path) return;
+        if (!$machine->technical_sheet) {
+            $this->addTechnicalSheet($machine, $path);
+            return;
+        }
+        if ($path == $machine->technical_sheet->path) return;
+        if (Storage::exists("public/" . $machine->technical_sheet->path)) Storage::delete("public/" . $machine->technical_sheet->path);
+        $machine->technical_sheet()->update(['path' => $path]);
     }
 
     /**
@@ -105,6 +125,7 @@ class MachineController extends Controller
 //          UPDATE MACHINE
             $machine->update($request->except(['articles', 'image']));
             $this->updateImage($machine, $request->image);
+            $this->updateTechnicalSheet($machine, $request->technical_sheet);
 //            ATTACH ARTICLES
             $articles = [];
             array_map(function ($article) use (&$articles) {
