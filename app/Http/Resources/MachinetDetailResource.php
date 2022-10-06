42b327915e6b198994ca27d5e2618b94f71c453b
Luis Murrugarra <luismurrugarra17@gmail.com>
2022-10-06 17:40:17 -0500

    UPDATE:WorkingSheetResource - get diff dates

--- app/Http/Resources/MachinetDetailResource.php
+++ app/Http/Resources/MachinetDetailResource.php
@@ -83,9 +83,9 @@ class MachinetDetailResource extends JsonResource
 
     function converterSecondsInTime($time_in_seconds)
     {
-        $hours = floor($time_in_seconds / 3600);
-        $minutes = floor(($time_in_seconds - ($hours * 3600)) / 60);
-        $seconds = $time_in_seconds - ($hours * 3600) - ($minutes * 60);
+        $hours = strval(floor($time_in_seconds / 3600));
+        $minutes = strval(floor(($time_in_seconds - ($hours * 3600)) / 60));
+        $seconds = strval($time_in_seconds - ($hours * 3600) - ($minutes * 60));
 
         return [$hours, $minutes, $seconds];
     }
--- app/Http/Resources/WorkingHourResource.php
+++ app/Http/Resources/WorkingHourResource.php
@@ -9,11 +9,28 @@ class WorkingHourResource extends JsonResource
     /**
      * Transform the resource into an array.
      *
-     * @param  \Illuminate\Http\Request  $request
+     * @param \Illuminate\Http\Request $request
      * @return array|\Illuminate\Contracts\Support\Arrayable|\JsonSerializable
      */
     public function toArray($request)
     {
-        return parent::toArray($request);
+        return [
+            'date_time_start' => $this->date_time_start,
+            'date_time_end' => $this->date_time_end,
+            'date_time_diff' => $this->date_time_diff(),
+
+        ];
+    }
+
+    function date_time_diff()
+    {
+        $datetime1 = date_create($this->date_time_start);
+        $datetime2 = date_create($this->date_time_end);
+        $interval = date_diff($datetime1, $datetime2);
+        return [
+            'hours' => $interval->format('%h'),
+            'minutes' => $interval->format('%i'),
+            'secons' => $interval->format('%s'),
+        ];
     }
 }
