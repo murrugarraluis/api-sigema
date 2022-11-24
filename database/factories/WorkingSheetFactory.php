c0f1c1a53b7ee10dbdf86f04daa48b6b3c48758c
Luis Murrugarra <luismurrugarra17@gmail.com>
2022-11-23 19:33:04 -0500

    UPDATE:WorkingSheetFactory - seeder

--- database/factories/WorkingSheetFactory.php
+++ database/factories/WorkingSheetFactory.php
@@ -8,19 +8,20 @@ use Illuminate\Database\Eloquent\Factories\Factory;
 
 class WorkingSheetFactory extends Factory
 {
-    /**
-     * Define the model's default state.
-     *
-     * @return array
-     */
-    public function definition()
-    {
-        $machine = Machine::inRandomOrder()->limit(1)->first();
-        return [
-            'date' => $this->faker->dateTimeBetween('-20 days', '-1 days'),
+	/**
+	 * Define the model's default state.
+	 *
+	 * @return array
+	 */
+	public function definition()
+	{
+		$machine = Machine::inRandomOrder()->limit(1)->first();
+		return [
+			'date' => $this->faker->dateTimeBetween('-20 days', '-1 days'),
 //            'date_end' => $this->faker->dateTimeBetween('-20 days', '-1 days'),
-            'description' => $this->faker->text(),
-            'machine_id' => $machine,
-        ];
-    }
+			'description' => $this->faker->text(),
+			'machine_id' => $machine,
+			'is_open' => false
+		];
+	}
 }
