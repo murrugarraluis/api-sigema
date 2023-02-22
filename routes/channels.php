df7f3aa0c34597fabbf808995f31c7f7f6097445
Luis Murrugarra <luismurrugarra17@gmail.com>
2023-02-21 23:50:39 -0500

    CHANGE: Pusher Notification

--- routes/channels.php
+++ routes/channels.php
@@ -12,11 +12,11 @@ use Illuminate\Support\Facades\Broadcast;
 | used to check if an authenticated user can listen to the channel.
 |
 */
-
-Broadcast::channel('App.Models.User.{id}', function ($user, $id) {
-    return (int) $user->id === (int) $id;
-});
-Broadcast::channel('notifications', function ($user, $id) {
-//	  echo $user;
-    return true;
-});
+//
+//Broadcast::channel('App.Models.User.{id}', function ($user, $id) {
+//    return (int) $user->id === (int) $id;
+//});
+//Broadcast::channel('notifications', function ($user, $id) {
+////	  echo $user;
+//    return true;
+//});
